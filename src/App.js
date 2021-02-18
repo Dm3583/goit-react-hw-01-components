0246ded56ff53a1ec8836bdf135b4ececf444e3b
Dm3583 <pankovdm@gmail.com>
2021-02-18 21:41:00 +0200

    linter test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
-import Profile from "./components/Profile/Profile";
-import Statistics from "./components/Statistics/Statistics"
-import user from "./user.json";
-import statisticalData from "./statistical-data.json";
-
+import React from 'react';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import user from './user.json';
+import statisticalData from './statistical-data.json';
 
 const App = () => (
     <div>
@@ -16,8 +15,8 @@ const App = () => (
             stats={user.stats}
         />
         <Statistics title="Upload stats" stats={statisticalData} />
-        {/* <Statistics stats={statisticalData} />
-    </div> */}
+        <Statistics stats={statisticalData} />
+    </div>
 );
 
 export default App;
